c0a9d80a6c7599ac0dc7a68f6dd23e7ea921a3bb
Paul A. Jungwirth <pj@illuminatedcomputing.com>
2014-03-24 21:39:10 -0700

    Permit a noop option for easier integration into non-production environments

--- lib/strongjob_client/client.rb
+++ lib/strongjob_client/client.rb
@@ -3,21 +3,29 @@ require 'faraday_middleware'
 module StrongjobClient
   class Client
 
-    def initialize(api_key)
-      @api_key = api_key
+    def initialize(options={})
+      @options = options
+      @api_key = options[:api_key]
+      raise ArgumentError, ":api_key is required to initialize the Strongjob Client" unless @api_key
     end
 
     def run(job_slug, options={}, &block)
-      r = ::StrongjobClient::Run.new(connection(options), job_slug)
-      r.start!
+      options = @options.merge(options)
+      if options[:noop]
+        return nil
 
-      begin
-        yield r if block
-        r.finish!
-      rescue ::Exception => e
-        r.fail!(e)
+      else
+        r = ::StrongjobClient::Run.new(connection(options), job_slug)
+        r.start!
+
+        begin
+          yield r if block
+          r.finish!
+        rescue ::Exception => e
+          r.fail!(e)
+        end
+        return r.result
       end
-      return r.result
     end
 
     def connection(options={})
